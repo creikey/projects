3376b153410215acb5ab7db19d95be42173c0f99
Cameron Reikes <cameronreikes@gmail.com>
2017-12-07 00:12:51 -0800

    Parses the first file you give it

--- src/main.c
+++ src/main.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
+#include "prj_parser.h"
 
 int main( int argc, char ** argv ) {
-  printf( "Hello, world\n" );
+  if( argc > 1 ) {
+    prj_file * info = parse_project_file( argv[1] );
+    printf( "Info size is %lu\n", info->size );
+    for( int i = 0; i < info->size; i++ ) {
+      printf( "File data for %d: %s\n", i, info->data[i] );
+    }
+    free_project_file( info );
+    return 0;
+  } else {
+    printf( "YOU MUST INPUT A FILE\n" );
+    return -1;
+  }
 }
